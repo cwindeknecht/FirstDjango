abecef090c559f812bd1a32998f76fa1221e8ab3
Cody Windeknecht <cmwindeknecht@gmail.com>
2018-05-21 18:50:07 -0400

    Added Another View
    The original is routed to polls/, the second is polls/other.

--- first/polls/views.py
+++ first/polls/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def otherPage(request):
+    return HttpResponse("Hello, world.  You're at another page.")
